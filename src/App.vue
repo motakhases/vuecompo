95359ea4de5fe5098f8fb4cc6cceec634b3f598e
Ari Shojaei <amnshojaei@gmail.com>
2021-08-21 13:32:21 +0430

    chore: add some new tailwind config for test

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
-  <div class="p-[36px]">
-    <Button
-      size="large"
+  <div class="p-[36px] dark:bg-black flex gap-[8px]">
+    <button class="z-button">
+      Button
+    </button>
+    <button
+      class="z-button"
+      disabled
     >
       Button
-    </Button>
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Button from './components/Button/index.vue';
 
 export default Vue.extend({
   name: 'App',
-  components: {
-    Button,
-  },
 });
 </script>
+
+<style lang="scss">
+.z-button {
+  // Default
+  @apply p-btn-md bg-primary-light text-white dark:bg-primary-dark dark:text-text;
+
+  // Hover
+  @apply hover:bg-primary-light-hover hover:dark:bg-primary-dark-hover;
+
+  // Active
+  @apply active:bg-primary-light-active active:dark:bg-primary-dark-active;
+
+  // Disabled
+  @apply disabled:bg-primary-light-disabled disabled:dark:bg-primary-dark-disabled;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,77 @@
 module.exports = {
   mode: 'jit',
+  darkMode: 'media',
   purge: [
     './public/**/*.html',
     './src/**/*.vue',
   ],
+  theme: {
+    // Breakpoints
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+
+    // Color pallete
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#fff',
+      black: '#19191A',
+      text: '#0A0A0A',
+      primary: {
+        light: {
+          DEFAULT: '#0A33FF',
+          hover: '#082BD5',
+          active: '#0722AA',
+          disabled: '#9DADFF',
+        },
+        dark: {
+          DEFAULT: '#90AEF4',
+          hover: '#C3D3F9',
+          active: '#D6E0FA',
+          disabled: '#404C68',
+        },
+      },
+    },
+
+    // Spacing [margin, padding, top, ...]
+    spacing: {
+      px: '1px',
+      'btn-md': '8px 16px',
+    },
+
+    // Radius
+    borderRadius: {
+      none: '0',
+      sm: '5px',
+    },
+
+    boxShadow: {},
+
+    container: {},
+
+    cursor: {},
+
+    fontFamily: {
+      sans: ['system-ui'],
+      serif: ['Georgia'],
+    },
+
+    fontSize: {
+      xs: ['12px', { lineHeight: '14px' }],
+      md: ['14px', { lineHeight: '16px' }],
+    },
+
+    fontWeight: {
+      normal: '400',
+      bold: '700',
+    },
+
+    gap: {},
+  },
+  variants: {},
 };
