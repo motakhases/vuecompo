3c616c7c73c5a4b491d6fcefa814287e63fc508b
Shayeste <sh.malekpour1996@gmail.com>
2021-09-15 10:37:13 -0700

    build (folder structure) : build folder and add styles

---
+++ src/components/TextBox/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="['zpl-textbox-group']">
+    <textarea
+      :class="['zpl-textbox', { error }]"
+      :value="value"
+      :rows="rowsNumber"
+      :disabled="disabled"
+      :maxlength="limit > 0 ? limit : undefined"
+      @input="onInput"
+      @focusout="onFocusOut"
+      @focusin="onFocusIn"
+    />
+    <!-- show when we have label -->
+    <label
+      v-if="label"
+      :class="['zpl-textbox-label', { activeLabel }]"
+    >
+      {{ label }}
+    </label>
+    <!-- show when we have hint text or limit -->
+    <div class="zpl-textbox-hint-box">
+      <!-- show when we have hint text -->
+      <span
+        v-if="hintText"
+        :class="['zpl-textbox-hint', hint]"
+      >
+        {{ hintText }}
+      </span>
+      <!-- show when we have limit -->
+      <span
+        v-if="limit"
+        class="zpl-textbox-hint-limit"
+      >
+        {{ limit }}/{{ value.length }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
