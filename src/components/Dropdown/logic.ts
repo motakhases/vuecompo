49da0cd3d9142b498d625e04cca8de424b22ea0d
Shayeste <sh.malekpour1996@gmail.com>
2021-09-21 18:30:08 -0700

    feat : hover action and key event functionality

--- src/components/Dropdown/logic.ts
+++ src/components/Dropdown/logic.ts
@@ -2,11 +2,8 @@ import Vue from 'vue';
 
 const ARROW_DOWN_KEYCODE = 40;
 const ARROW_UP_KEYCODE = 38;
-const ARROW_LEFT_KEYCODE = 37;
-const ARROW_RIGHT_KEYCODE = 39;
 const ENTER_KEYCODE = 13;
-const TAB_KEYCODE = 9;
-
+const keyList = [9, 13, 16, 17, 18, 20, 32, 37, 38, 39, 40, 46];
 export default Vue.extend({
   name: 'Dropdown',
   props: {
@@ -50,12 +47,12 @@ export default Vue.extend({
   data() :{
     activeLabel: boolean;
     focused: boolean;
-    searchIndex: number;
+    activeOptionIndex: number;
     filteredOptions: Array<any>;} {
     return {
       activeLabel: !!this.value.length,
       focused: false,
-      searchIndex: -1,
+      activeOptionIndex: -1,
       filteredOptions: this.options,
     };
   },
@@ -90,7 +87,7 @@ export default Vue.extend({
         this.activeLabel = false;
       }
       // to remove active class
-      this.searchIndex = -1;
+      this.activeOptionIndex = -1;
     },
     toEnNumber(str: string) {
       // change all the Persian or Arabic numbers to English
@@ -138,62 +135,103 @@ export default Vue.extend({
       }
     },
     onKeyUp(e:any) {
-      const isEnterKey = e.keyCode === ENTER_KEYCODE;
-      const isArrowDownKey = e.keyCode === ARROW_DOWN_KEYCODE;
-      const isArrowUpKey = e.keyCode === ARROW_UP_KEYCODE;
-      const isTabKey = e.keyCode === TAB_KEYCODE;
-      const isArrowLeftKey = e.keyCode === ARROW_LEFT_KEYCODE;
-      const isArrowRightKey = e.keyCode === ARROW_RIGHT_KEYCODE;
-
-      if (isEnterKey
-        || isArrowDownKey
-        || isArrowUpKey
-        || isTabKey
-        || isArrowLeftKey
-        || isArrowRightKey) {
+      // if any key in the list is pressed do nothing
+      if (keyList.includes(e.keyCode)) {
         return;
       }
-      if (this.value.length) {
-        this.filteredOptions = this.options.filter(
-          (option:any) => option.name.toLowerCase().includes(this.value),
-        );
-      } else {
-        this.filteredOptions = this.options;
-      }
+      // otherwise filter the list based on value that user is typing
+      this.filteredOptions = this.options.filter(
+        (option:any) => option.name.toLowerCase().includes(this.value.toLowerCase()),
+      );
     },
     onKeyDown(e:any) {
       const isArrowDownKey = e.keyCode === ARROW_DOWN_KEYCODE;
       const isArrowUpKey = e.keyCode === ARROW_UP_KEYCODE;
       const isEnterKey = e.keyCode === ENTER_KEYCODE;
+
+      // if dropdown is open
       if (this.focused) {
-        if (isArrowDownKey) {
-          if (this.searchIndex < 0) {
-            this.searchIndex = 0;
-          } else if (this.searchIndex < this.filteredOptions.length - 1) {
-            this.searchIndex += 1;
-          } else if (this.searchIndex >= this.filteredOptions.length - 1) {
-            this.searchIndex = 0;
-          }
-        } else if (isArrowUpKey) {
-          if (this.searchIndex < 0) {
-            this.searchIndex = 0;
-          } else if (this.searchIndex === 0) {
-            this.searchIndex = this.filteredOptions.length - 1;
-          } else if (this.searchIndex <= this.filteredOptions.length - 1) {
-            this.searchIndex -= 1;
-          }
-        } else if (isEnterKey) {
-          if (this.searchIndex >= 0) {
-            if (this.filteredOptions.length) {
-              const newValue = this.filteredOptions[this.searchIndex].name;
-              this.$emit('input', this.toEnNumber(newValue));
-              this.hideOptions();
+        // if arrow up or down key is pressed
+        if (isArrowDownKey || isArrowUpKey) {
+          for (let i = 0; i < this.filteredOptions.length; i += 1) {
+            // find the active option based on class
+            const activeOption = (this.$refs.optionRef[i]).classList.contains('active');
+
+            // if there is active option then remove the active class and
+            // update the index of active option in order to move active class
+            if (activeOption) {
+              this.activeOptionIndex = i;
+              (this.$refs.optionRef[i]).classList.remove('active');
             }
           }
         }
+        // if arrow down key is pressed
+        if (isArrowDownKey) {
+          // if there's no active index so the first index is active
+          if (this.activeOptionIndex === -1) {
+            this.activeOptionIndex = 0;
+
+            // active next option till the end of list
+          } else if (this.activeOptionIndex < this.filteredOptions.length - 1) {
+            this.activeOptionIndex += 1;
+
+            // if we achieved to the end of list so go to the first option
+          } else if (this.activeOptionIndex >= this.filteredOptions.length - 1) {
+            this.activeOptionIndex = 0;
+          }
+          // if arrow up key is pressed
+        } else if (isArrowUpKey) {
+          // if there's no active index so the first index is active
+          if (this.activeOptionIndex === -1) {
+            this.activeOptionIndex = 0;
+
+            // if we achieved to the first of list so go to the last option
+          } else if (this.activeOptionIndex === 0) {
+            this.activeOptionIndex = this.filteredOptions.length - 1;
+
+            // active previous option till the start of list
+          } else if (this.activeOptionIndex <= this.filteredOptions.length - 1) {
+            this.activeOptionIndex -= 1;
+          }
+
+          // if enter key is pressed
+        } else if (isEnterKey) {
+          // if there's active option and we have filter list
+          if (this.activeOptionIndex >= 0 && this.filteredOptions) {
+            // take the name of active option
+            const newValue = this.filteredOptions[this.activeOptionIndex]
+              ? this.filteredOptions[this.activeOptionIndex].name : '';
+
+            // update the value of input
+            this.$emit('input', this.toEnNumber(newValue));
+
+            // close the dropdown
+            this.hideOptions();
+
+            // disable the active option
+            this.activeOptionIndex = -1;
+          }
+        }
+        // if dropdown is closed and enter key is pressed
       } else if (isEnterKey) {
+        // open the dropdown
         this.showOptions();
+
+        // show the compelete list
+        this.filteredOptions = this.options;
       }
+    },
+    activateOption(e:any) {
+      // if any option has active class remove it
+      [].forEach.call(this.$refs.optionRef, (el:any) => {
+        el.classList.remove('active');
+      });
+      // add active class when a mouse enters
+      e.target.classList.add('active');
+    },
+    deactivateOption(e:any) {
+      // remove active class when a mouse leaves
+      e.target.classList.remove('active');
     },
   },
 });
