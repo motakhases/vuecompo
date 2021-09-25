503a34a2d1c5da813f31a403749a84b083b9538d
Ari Shojaei <amnshojaei@gmail.com>
2021-09-25 17:24:21 +0330

    fix: update app.vue and link style

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,22 @@
 <template>
-  <div
-    dir="rtl"
-    class="p-4xl bg-[#c1e0c1]"
-  >
-    <CheckBox
-      v-model="selctedCheck"
-      :options="testChecks"
-      name="test"
-    />
-    <br>
-    <br>
-    <Radio
-      v-model="selectedRadio"
-      :options="testChecks"
-      name="test"
-    />
-    <br>
-    <br>
-    <Tag :btn="true">
-      برچسب
-    </Tag>
-    <br>
-    <br>
-    <br>
-    <br>
-    <tabs>
-      <tab title="۱لیبل تب" >Hello From Tab 1</tab>
-      <tab title="۲لیبل تب" warn>Hello From Tab 2</tab>
-      <tab title="۳لیبل تب" warn>Hello From Tab 3</tab>
-      <tab title="۴لیبل تب">Hello From Tab 4</tab>
-    </tabs>
-    <br>
-    <br>
-    <br>
-    <tabs :fill-container="true">
-      <tab title="۱لیبل تب">Hello From Tab 1</tab>
-      <tab title="۲لیبل تب">Hello From Tab 2</tab>
-      <tab title="۳لیبل تب">Hello From Tab 3</tab>
-      <tab title="۴لیبل تب">Hello From Tab 4</tab>
-    </tabs>
+  <div class="w-full mx-auto max-w-[1024px] bg-secondary rounded-md p-xl mt-2xl">
+    <h1 class="font-bold text-ls-md mb-sm">
+      @zarinpal/web-components
+    </h1>
+    <p>
+      This package providing a new design system for <a href="https://zarinpal.com/">ZarinPal</a> that contains a foundation and many components based on
+      the new design system. although this package developing for ZarinPal, it will use for other ZarinPal products in
+      the future. In order to increase development speed and consistency on the style guide, we decided to use
+      TailwindCSS. So this package uses <a href="https://tailwindcss.com/">TailwindCSS</a>, <a href="https://www.typescriptlang.org/">Typescript</a>
+      and <a href="https://vuejs.org/">Vuejs</a>.
+    </p>
   </div>
 </template>
 
 <script lang="js">
 import Vue from 'vue';
-import CheckBox from './components/CheckBox/index.vue';
-import Radio from './components/Radio/index.vue';
-import Tag from './components/Tag/index.vue';
-import tabs from './components/Tabs/index.vue';
-import tab from './components/Tabs/Tab/index.vue';
 
 export default Vue.extend({
   name: 'App',
-  components: {
-    CheckBox, Tag, tabs, tab, Radio,
-  },
-  data() {
-    return {
-      testChecks: [
-        { label: 'تسس', value: 'test' },
-        { label: 'تست', value: 'testt' },
-        { label: 'تسستس', value: 'testttt' },
-      ],
-      selctedCheck: [],
-      selectedRadio: 'test',
-    };
-  },
-  methods: {
-    getActiveTab(tabName) {
-      this.activeTab = tabName;
-    },
-  },
 });
 </script>
