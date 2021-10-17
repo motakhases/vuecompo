880de19366a526b5f28f04a95ed1d1e7749f9f23
Shayeste <sh.malekpour1996@gmail.com>
2021-10-17 15:18:36 -0700

    feat: create folder structures

---
+++ src/components/TopBar/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="rtl">
+    <div class="zpl-topbar-container">
+      <div class="zpl-topbar-right-side">
+        <Icon
+          name="angleDown"
+          class="zpl-topbar-menu"
+        />
+
+        <div>
+          <a href="#1">logo</a>
+        </div>
+      </div>
+      <div class="zpl-topbar-right-side">
+        fd
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
