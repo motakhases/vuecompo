0aae493229e6ea5f2512d92a2e0b4de089fa753a
Shayeste <sh.malekpour1996@gmail.com>
2021-10-19 16:03:11 -0700

    feat: add some tests

--- src/components/Logo/index.unit.test.js
+++ src/components/Logo/index.unit.test.js
@@ -7,50 +7,16 @@ describe('Logo', () => {
   it('has default structure', async () => {
     const wrapper = shallowMount(Logo);
 
-    expect(wrapper.element.tagName).toBe('a');
-    expect(wrapper.attributes('href')).toBeDefined();
+    expect(wrapper.element.tagName).toBe('DIV');
+    expect(wrapper.attributes('language')).toBeDefined();
   });
 
-  it('renders default slot content', async () => {
-    const wrapper = shallowMount(Logo, {
-      slots: {
-        default: '<span>link</span>',
-      },
-    });
-
-    expect(wrapper.find('span').exists()).toBe(true);
-    expect(wrapper.text()).toBe('link');
-  });
-
-  it('has attribute disabled when disabled set', () => {
+  it('has medium class when size is set to medium', () => {
     const wrapper = shallowMount(Logo, {
       propsData: {
-        disabled: true,
+        size: 'medium',
       },
     });
-    expect(wrapper.attributes('disabled')).toBeDefined();
-  });
-
-  it('renders icon on the left when leftIcon has been set', () => {
-    const wrapper = shallowMount(Logo, {
-      propsData: {
-        leftIcon: 'Icon-Angle-Large-Left',
-      },
-    });
-    const icon = wrapper.find('.Icon-Angle-Large-Left');
-
-    expect(icon.exists()).toBe(true);
-  });
-
-  it('renders loading and doesnt show slot when loading is true', () => {
-    const wrapper = shallowMount(Logo, {
-      propsData: {
-        loading: true,
-      },
-    });
-    const loading = wrapper.find('.zpl-Logo-loading');
-
-    expect(loading.exists()).toBe(true);
-    expect(wrapper.text()).toBe('');
+    expect(wrapper.classes()).toContain('medium');
   });
 });
