8028e1f90e78eedc4ba3e2dce8838e617ba961ed
Shayeste <sh.malekpour1996@gmail.com>
2021-10-20 11:28:26 -0700

    feat: add logo and icon components

--- src/components/TopBar/logic.ts
+++ src/components/TopBar/logic.ts
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Icon from '../Icon/index.vue';
+import Logo from '../Logo/index.vue';
 
 export default Vue.extend({
   name: 'TopBar',
-  components: { Icon },
+  components: { Icon, Logo },
 });
