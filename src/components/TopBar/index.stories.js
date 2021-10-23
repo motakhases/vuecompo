b6fb6b79f1075454d26802bab6ae2dcc39fc4fe1
Shayeste <sh.malekpour1996@gmail.com>
2021-10-23 12:50:27 -0700

    feat: add UserProfile component

--- src/components/TopBar/index.stories.js
+++ src/components/TopBar/index.stories.js
@@ -1,37 +1,30 @@
 import Component from "./index.vue";
 import Logo from "../Logo/index.vue";
-import Icon from "../Icon/index.vue"
+import Icon from "../Icon/index.vue";
+import UserProfile from "../UserProfile/index.vue";
+
 export default {
   component: Component,
   title: "Components/TopBar",
 };
 
 const Template = (_, { argTypes }) => ({
-  components: { Component, Logo, Icon },
+  components: { Component, Logo, Icon, UserProfile },
   props: Object.keys(argTypes),
   template: `
-		<Component>
-		<template v-slot:right-side>
-		<Icon
-          name="menuBurger"
-          class="zpl-topbar-menu"
-        />
-				<div class="zpl-topbar-logo">
-						<Logo size="medium" name="zarinpal" type="logo" language="fa" />
-
-				</div>
-		</template>
-		<template v-slot:left-side>
-		<Icon
-          name="questionCircle"
-          class="zpl-topbar-menu"
-        />
-				<Icon
-				name="notification"
-				class="zpl-topbar-menu"
-			/>
-		</template>
-	</Component>
+  <Component>
+    <template v-slot:right-side>
+      <Icon name="menuBurger" class="zpl-topbar-menu" />
+      <div class="zpl-topbar-logo">
+        <Logo size="medium" name="zarinpal" type="logo" language="fa" />
+      </div>
+    </template>
+    <template v-slot:left-side>
+      <Icon name="questionCircle" class="zpl-topbar-menu" />
+      <Icon name="notification" class="zpl-topbar-menu" />
+      <UserProfile username="نام کاربر" />
+    </template>
+  </Component>
   `,
 });
 
