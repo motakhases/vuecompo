4764f6e29abd782be3a166b1c815bd919370a674
Shayeste <sh.malekpour1996@gmail.com>
2021-10-27 12:06:32 -0700

    feat: add Create component and its styles

---
+++ src/components/NavigationBar/Create/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="rtl">
+    <div class="zpl-nav-create">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
