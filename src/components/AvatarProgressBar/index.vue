6692832bd7f6badb24b490c991ec7e7a6944e4f9
Shayeste <sh.malekpour1996@gmail.com>
2021-11-14 11:00:07 -0800

    feat: create folder

---
+++ src/components/AvatarProgressBar/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    :class="['zpl-avatar-progress', size, type, { fill }]"
+    data-percentage="50"
+  >
+    <svg
+      viewBox="0 0 33.83098862 33.83098862"
+      xmlns="http://www.w3.org/2000/svg"
+    ><circle
+      class="circle-chart__background"
+      cx="50%"
+      cy="50%"
+      r="calc(50% - 1px)"
+    /><circle
+      class="circle-chart__circle success-stroke"
+      stroke-dasharray="90,100"
+      cx="50%"
+      cy="50%"
+      r="calc(50% - 1px)"
+    /></svg>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
