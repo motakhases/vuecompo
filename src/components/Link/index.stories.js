b08e37149c9f1b528fe732320fb25c2fa141083f
Ari Shojaei <amnshojaei@gmail.com>
2021-11-15 11:57:28 +0330

    fix(Link): storybook styling

--- src/components/Link/index.stories.js
+++ src/components/Link/index.stories.js
@@ -15,21 +15,25 @@ const Template = (_, { argTypes }) => ({
   components: { Link },
   props: Object.keys(argTypes),
   template: `
-		<Link
+  <div class="dark:bg-surface-dark dark:text-text-dark p-sm rounded-md" dir="rtl">
+    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از 
+    <Link
       :to="to"
       :href="href"
       :exact="exact"
     >
       {{ text }}
     </Link>
+    و با استفاده از طراحان گرافیک است.
+  </div>
   `,
 });
 
 export const Default = Template.bind({});
 
 Default.args = {
-  text: "لینک",
+  text: "صنعت چاپ",
   to: "",
-  href: "#",
+  href: "https://fa.wikipedia.org/wiki/%DA%86%D8%A7%D9%BE",
   exact: false,
 };
