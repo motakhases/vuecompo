9a72010d7b9a1fdfaedf28d493e3bf3e4b5ef45a
Shayeste <sh.malekpour1996@gmail.com>
2021-11-20 12:05:31 -0800

    feat: create user level component

--- src/components/UserProfileCard/index.stories.js
+++ src/components/UserProfileCard/index.stories.js
@@ -2,13 +2,15 @@ import UserProfileCard from "./index.vue";
 import CheckMark from "./CheckMark/index.vue";
 import TaskAction from "./TaskAction/index.vue";
 import ZarinID from "./ZarinID/index.vue";
+import UserLevel from "./UserLevel/index.vue";
+import Label from "@/components/Label/index.vue";
 export default {
   component: UserProfileCard,
   title: "Components/UserProfileCard",
 };
 
 const Template = (_, { argTypes }) => ({
-  components: { UserProfileCard, CheckMark, TaskAction, ZarinID },
+  components: { UserProfileCard, CheckMark, TaskAction, ZarinID, UserLevel,Label },
   props: Object.keys(argTypes),
   template: `
   <div class="dark:bg-surface-dark p-sm rounded-md">
@@ -27,7 +29,18 @@ task
 			<CheckMark complete/>
 			<CheckMark/>
 		</div>
+<UserLevel title="عنوان سطح عضویت">
 
+<Label type="outlined" size="medium">
+حقوقی
+</Label>
+<Label type="positive" size="medium">
+VIP
+</Label>
+<Label type="negative" size="medium">
+توقیف شده
+</Label>
+</UserLevel>
 		</UserProfileCard>
   </div>
   `,
