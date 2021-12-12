2731f5cc331d87771531fcea6ca7413a20d34a62
kasradara <kasradara@outlook.com>
2021-12-12 13:29:03 +0330

    feat(pagination): convert pagination js file to ts

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -90,89 +90,9 @@
 
 <script>
 
+import Vue from 'vue';
+import logic from './logic';
 import './style.scss';
-import Button from '@/components/Button/index.vue';
-import Popover from '@/components/Popover/index.vue';
 
-export default {
-  components: { Button, Popover },
-  props: {
-    initPage: {
-      type: Number,
-      default: 1,
-    },
-    initLimit: {
-      type: Number,
-      default: 4,
-    },
-    lastPage: {
-      type: Number,
-      default: 10,
-    },
-  },
-  data() {
-    return {
-      limit: null,
-      page: null,
-      limitBox: false,
-      pageBox: false,
-    };
-  },
-  computed: {
-    limitsList() {
-      const list = [];
-      for (let limit = 4; limit <= 24;) {
-        list.push(
-          {
-            title: limit.toString(),
-            value: limit,
-          },
-        );
-        limit += 4;
-      }
-      return list;
-    },
-    pagesList() {
-      const list = [];
-      // eslint-disable-next-line no-plusplus
-      for (let page = 1; page <= this.lastPage; page++) {
-        list.push(
-          {
-            title: page.toString(),
-            value: page,
-          },
-        );
-      }
-      return list;
-    },
-    period() {
-      const all = this.lastPage * this.limit;
-      const to = this.page * this.limit;
-      const from = to - (this.limit - 1);
-      return `${from} - ${to} از ${all} نتیجه`;
-    },
-  },
-  created() {
-    this.limit = this.initLimit;
-    this.page = this.initPage;
-  },
-  methods: {
-    changeLimit(newLimit) {
-      this.limit = newLimit;
-      this.limitBox = false;
-      this.$emit('paginate', {
-        page: 1,
-        limit: newLimit,
-      });
-    },
-    changePage(newPage) {
-      this.page = newPage;
-      this.pageBox = false;
-      this.$emit('paginate', {
-        page: newPage,
-        limit: this.limit,
-      });
-    },
-  },
-};
+export default Vue.extend({ mixins: [logic] });
 </script>
