33cc763b09870f343267438ba4429dce45a2302f
Shayeste <sh.malekpour1996@gmail.com>
2021-12-13 15:28:25 -0800

    feat: create folder

---
+++ src/components/Status/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    :class="['zpl-status', type ]"
+    dir="rtl"
+  >
+    <span
+      v-if="indicator"
+      class="circle"
+    />
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
