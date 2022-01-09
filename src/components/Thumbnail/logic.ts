a36c34ef5cde5db0042a7813dec5294455c8e2d2
Shayeste <46010510+shayeste1996@users.noreply.github.com>
2022-01-09 12:14:38 +0330

    fix: change exclamation mark to question

--- src/components/Thumbnail/logic.ts
+++ src/components/Thumbnail/logic.ts
@@ -6,12 +6,12 @@ import BankLogos from '@/components/BankLogos/index.vue';
   components: { Icon, BankLogos },
 })
 export default class Thumbnail extends Vue {
-  @Prop({ type: String }) readonly icon!: string;
+  @Prop({ type: String }) readonly icon?: string;
 
-  @Prop({ type: String }) readonly logo!: string;
+  @Prop({ type: String }) readonly logo?: string;
 
-  @Prop({ type: String }) readonly img!: string;
+  @Prop({ type: String }) readonly img?: string;
 
-  @Prop({ type: String }) readonly alt!: string;
+  @Prop({ type: String }) readonly alt?: string;
 }
 
