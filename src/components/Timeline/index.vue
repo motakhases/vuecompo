d6fba042464a8a4e4d32f14d36ad27e6b2cd7983
Shayeste <46010510+shayeste1996@users.noreply.github.com>
2022-01-24 16:07:10 +0330

    feat: create Timeline component

---
+++ src/components/Timeline/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="zpl-timeline">
+    <div class="zpl-timeline-icon">
+      <Icon :name="icon" />
+    </div>
+    <div class="zpl-timeline-cart">
+      <div class="zpl-timeline-title">
+        <span>{{ title }}</span>
+        <Label
+          v-if="labelText"
+          size="small"
+          :type="labelType"
+          :text="labelText"
+        />
+      </div>
+      <div class="zpl-timeline-sub">
+        {{ subText }}
+      </div>
+      <div class="zpl-timeline-sub">
+        {{ date }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
