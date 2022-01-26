dba857b79e9271de7bc3fcf2a9ae25480920415d
kasradara <kasradara@outlook.com>
2022-01-26 12:10:33 +0330

    fix(pageHeading) : classbased done , statuses added , button are slot

--- src/components/PageHeading/index.vue
+++ src/components/PageHeading/index.vue
@@ -8,15 +8,22 @@
       class="return-btn"
       @click="router.go(-1)"
     >
-      <Icon
-        name="arrowRight"
-        class="return-icon"
+      <Button
+        icon="ArrowRight"
+        type="tertiary"
       />
     </div>
     <div class="heading-content">
       <span class="title">
         {{ title }}
       </span>
+      <div
+        v-for="(status, index) in statuses"
+        :key="index"
+        class="statuses"
+      >
+        <Status v-bind="status" />
+      </div>
       <p
         v-show="desc.length>0 && !stickHeading"
         class="desc"
@@ -25,15 +32,9 @@
       </p>
     </div>
     <div
-      v-if="hasButton"
       class="heading-btn"
     >
-      <btn
-        :on-click="btnOperate"
-        v-bind="btn"
-      >
-        {{ btn.label }}
-      </btn>
+      <slot name="buttons" />
     </div>
   </div>
 </template>
--- src/components/PageHeading/logic.ts
+++ src/components/PageHeading/logic.ts
@@ -1,59 +1,39 @@
-import Vue from 'vue';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 import Icon from '../Icon/index.vue';
-import btn from '../Button/index.vue';
+import Status from '../Status/index.vue';
+import Button from '../Button/index.vue';
 
-export default Vue.extend({
-  name: 'PageHeading',
-  components: { Icon, btn },
-  props: {
-    returnBTN: {
-      type: Boolean,
-      default: true,
-    },
-    title: {
-      type: String,
-      default: '',
-    },
-    desc: {
-      type: String,
-      default: '',
-    },
-    sticky: {
-      type: Boolean,
-      default: false,
-    },
-    hasButton: {
-      type: Boolean,
-      default: false,
-    },
-    btn: {
-      // set btn options like label , type , disable & etc
-      type: Object,
-      default: () => ({}),
-    },
+@Component({
+  components: {
+    Icon, Status, Button,
   },
-  data() {
-    return {
-      stickHeading: false,
-    };
-  },
-  mounted() {
+})
+export default class PageHeading extends Vue {
+  @Prop({ type: String, required: true }) readonly title!: string
+
+  @Prop({ type: String }) readonly desc!: string
+
+  @Prop({ type: Boolean, default: false }) readonly returnBTN!: boolean
+
+  @Prop({ type: Boolean, default: false }) readonly sticky!: boolean
+
+  @Prop({ type: Array, default: [] }) readonly statuses!: boolean
+
+  stickHeading = false
+
+  checkScrolling(): void {
+    const element = this.$refs.pageHeading as HTMLElement;
+    const distanceOfTop = element.getBoundingClientRect().top;
+    if (distanceOfTop <= 0) {
+      this.stickHeading = true;
+    } else if (window.scrollY === 0) {
+      this.stickHeading = false;
+    }
+  }
+
+  mounted(): void {
     if (this.sticky) {
       document.addEventListener('scroll', this.checkScrolling);
     }
-  },
-  methods: {
-    checkScrolling() {
-      const element = this.$refs.pageHeading as HTMLElement;
-      const distanceOfTop = element.getBoundingClientRect().top;
-      if (distanceOfTop <= 0) {
-        this.stickHeading = true;
-      } else if (window.scrollY === 0) {
-        this.stickHeading = false;
-      }
-    },
-    btnOperate() {
-      this.$emit('btnClick');
-    },
-  },
-});
+  }
+}
