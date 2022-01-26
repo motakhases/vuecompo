9f22483b99626427729fba1ea02709ee919b0134
kasradara <kasradara@outlook.com>
2022-01-26 10:49:27 +0330

    fix(stories fixed)

--- stories/nav.stories.js
+++ stories/nav.stories.js
@@ -3,6 +3,6 @@ export default {
 };
 
 export { default as NavigationBar } from '@/components/NavigationBar/index.stories.js';
-// export { default as PageHeading } from '@/components/PageHeading/index.stories.js';
+export { default as PageHeading } from '@/components/PageHeading/index.stories.js';
 export { default as TopBar } from '@/components/TopBar/index.stories.js';
 
