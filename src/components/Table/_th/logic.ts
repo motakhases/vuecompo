bbb794b49a77556ff906c3ef3a3212f61735bf9c
Ari Shojaei <amnshojaei@gmail.com>
2022-01-31 10:12:06 +0330

    feat(Table): add selecting feature

--- src/components/Table/_th/logic.ts
+++ src/components/Table/_th/logic.ts
@@ -2,14 +2,27 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 // Components
 import Label from '@/components/Label/index.vue';
+import Icon from '@/components/Icon/index.vue';
 
 @Component({
   components: {
     Label,
+    Icon,
   },
 })
 export default class Logic extends Vue {
   @Prop({ type: String }) readonly title?: string
 
   @Prop({ type: String }) readonly label?: string
+
+  sortIcons = {
+    ascending: 'FilledArrowUp',
+    descending: 'FilledArrowDown',
+  }
+
+  sortOrder = null
+
+  sort(): void {
+    console.log('a');
+  }
 }
