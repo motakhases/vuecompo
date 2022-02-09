9ab2bbb0fc557720bd1bef47284fe260e99407b4
Ari Shojaei <amnshojaei@gmail.com>
2022-02-09 10:08:09 +0330

    fix: navigationBar toggle

--- src/components/NavigationBar/index.vue
+++ src/components/NavigationBar/index.vue
@@ -1,53 +1,60 @@
 <template>
-  <nav :class="['zpl-nav-bar-container', { toggle }]">
-    <!-- Logo -->
-    <div class="zpl-nav-bar-logo">
-      <Logo
-        name="zarinpal"
-        type="logo"
-        language="fa"
-        size="medium"
+  <div>
+    <nav :class="['zpl-nav-bar-container', { 'show': isShow }]">
+      <!-- Logo -->
+      <div class="zpl-nav-bar-logo">
+        <Logo
+          name="zarinpal"
+          type="logo"
+          language="fa"
+          size="medium"
+        />
+      </div>
+
+      <!-- Terminals -->
+      <SwitchTerminalPopover
+        :terminals="terminals"
+        :active-terminal="activeTerminal"
       />
-    </div>
 
-    <!-- Terminals -->
-    <SwitchTerminalPopover
-      :terminals="terminals"
-      :active-terminal="activeTerminal"
+      <!-- Nav item links -->
+      <div class="zpl-nav-bar-content">
+        <ul
+          v-if="aboveLinks.length"
+          class="zpl-nav-bar-list has-border"
+        >
+          <NavItem
+            v-for="(link, index) in aboveLinks"
+            :key="index"
+            :active="link.active"
+            :link="link.link"
+            :title="link.title"
+            :icon="link.icon"
+            :badge="link.badge"
+          />
+        </ul>
+        <ul
+          v-if="belowLinks && belowLinks.length"
+          class="zpl-nav-bar-list"
+        >
+          <NavItem
+            v-for="(link, index) in belowLinks"
+            :key="index"
+            :active="link.active"
+            :link="link.link"
+            :title="link.title"
+            :icon="link.icon"
+            :badge="link.badge"
+          />
+        </ul>
+      </div>
+    </nav>
+
+    <div
+      :class="['zpl-nav-overlay', { 'show': isShow }]"
+      @click="toggle"
     />
-
-    <!-- Nav item links -->
-    <div class="zpl-nav-bar-content">
-      <ul
-        v-if="aboveLinks.length"
-        class="zpl-nav-bar-list has-border"
-      >
-        <NavItem
-          v-for="(link, index) in aboveLinks"
-          :key="index"
-          :active="link.active"
-          :link="link.link"
-          :title="link.title"
-          :icon="link.icon"
-          :badge="link.badge"
-        />
-      </ul>
-      <ul
-        v-if="belowLinks && belowLinks.length"
-        class="zpl-nav-bar-list"
-      >
-        <NavItem
-          v-for="(link, index) in belowLinks"
-          :key="index"
-          :active="link.active"
-          :link="link.link"
-          :title="link.title"
-          :icon="link.icon"
-          :badge="link.badge"
-        />
-      </ul>
-    </div>
-  </nav>
+  </div>
 </template>
 
 <script lang="ts">
