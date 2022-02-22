b6fc2199b018db02d7ea9057ea2eebabaf840f62
Ari Shojaei <amnshojaei@gmail.com>
2022-02-22 15:53:46 +0330

    feat: add sub social link component

---
+++ src/components/Share/_social/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="zpl-social">
+    <!-- Icon -->
+    <a
+      :href="socials[keyName]"
+      target="_blank"
+      class="zpl-social-icon"
+    >
+      <Icon
+        :name="keyName"
+        class="_icon"
+      />
+    </a>
+
+    <!-- Name -->
+    <span class="zpl-social-name">
+      {{ name }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
