f2b79c5f5ec6e432dbe2827a2a96311a7d96eb8d
Shayeste <46010510+shayeste1996@users.noreply.github.com>
2022-03-13 17:44:22 +0330

    fix: fix z-index of modal, pageHeading and modal width

--- src/components/Modal/logic.ts
+++ src/components/Modal/logic.ts
@@ -6,13 +6,17 @@ import Button from '@/components/Button/index.vue';
   components: { Icon, Button },
 })
 export default class Modal extends Vue {
-  @Prop({ type: String, required: true }) readonly title!: string
+  @Prop({ type: String, required: true }) readonly title!: string;
 
-  @Prop({ type: Boolean, default: false }) readonly isOpen!: boolean
+  @Prop({ type: Boolean, default: false }) readonly isOpen!: boolean;
 
-  @Prop({ type: Function }) toggle!: () => boolean
+  @Prop({ type: Function }) toggle!: () => boolean;
 
-  @Prop({ type: Boolean }) closable!: boolean
+  @Prop({ type: Boolean }) closable!: boolean;
 
-  @Prop({ type: String }) readonly width?: string
+  @Prop({ type: String }) readonly width?: string;
+
+  get finalWidth(): string {
+    return `w-[${this.width}]`;
+  }
 }
