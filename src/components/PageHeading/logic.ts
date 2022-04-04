f4f31e4985e24e8f8e93cb63ef39cd67845086de
Shayeste <46010510+shayeste1996@users.noreply.github.com>
2022-04-04 11:29:01 +0430

    fix: fix page heading issue

--- src/components/PageHeading/logic.ts
+++ src/components/PageHeading/logic.ts
@@ -1,4 +1,6 @@
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import {
+  Component, Prop, Vue, Ref,
+} from 'vue-property-decorator';
 import { PageHeadingStatus } from '@/types';
 import Icon from '../Icon/index.vue';
 import Status from '../Status/index.vue';
@@ -6,29 +8,34 @@ import Button from '../Button/index.vue';
 
 @Component({
   components: {
-    Icon, Status, Button,
+    Icon,
+    Status,
+    Button,
   },
 })
 export default class PageHeading extends Vue {
-  @Prop({ type: String, required: true }) readonly title!: string
+  @Prop({ type: String, required: true }) readonly title!: string;
 
-  @Prop({ type: String }) readonly desc?: string
+  @Prop({ type: String }) readonly desc?: string;
 
-  @Prop({ type: Boolean, default: false }) readonly returnBtn!: boolean
+  @Prop({ type: Boolean, default: false }) readonly returnBtn!: boolean;
 
-  @Prop({ type: Boolean, default: false }) readonly sticky!: boolean
+  @Prop({ type: Boolean, default: false }) readonly sticky!: boolean;
 
-  @Prop({ type: Array }) readonly statuses?: PageHeadingStatus[]
+  @Prop({ type: Array }) readonly statuses?: PageHeadingStatus[];
 
-  stickHeading = false
+  @Ref('pageHeading') readonly pageHeading!: HTMLElement;
+
+  stickHeading = false;
 
   checkScrolling(): void {
-    const element = this.$refs.pageHeading as HTMLElement;
-    const distanceOfTop = element.getBoundingClientRect().top;
-    if (distanceOfTop <= 56) {
-      this.stickHeading = true;
-    } else {
-      this.stickHeading = false;
+    const distanceOfTop = this.pageHeading?.getBoundingClientRect()?.top;
+    if (this.pageHeading) {
+      if (distanceOfTop <= 56) {
+        this.stickHeading = true;
+      } else {
+        this.stickHeading = false;
+      }
     }
   }
 
