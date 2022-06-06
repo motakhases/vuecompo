0b2d9d3a0345298b58f1da0d8383adb9a4bedd29
Ari Shojaei <amnshojaei@gmail.com>
2022-06-06 15:14:41 +0430

    feat: add accordion component

---
+++ src/components/Accordion/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="zpl-accordion">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
