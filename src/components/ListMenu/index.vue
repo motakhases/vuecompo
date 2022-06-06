a62a11e5f714e15159c2116916cc6abcf5b70097
Ari Shojaei <amnshojaei@gmail.com>
2022-06-06 16:18:16 +0430

    feat: add ListMenu components

---
+++ src/components/ListMenu/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="zpl-list-menu">
+    <button
+      v-for="(item, i) in items"
+      :key="i"
+      :class="['zpl-list-menu-item', { 'active': item.active }]"
+      @click="itemClicked(item.id)"
+    >
+      {{ item.title }}
+
+      <div>
+        <span class="amount" v-if="item.amount && item.amount > 0">
+          ({{ item.amount }})
+        </span>
+
+        <Icon class="icon" name="AngleLeft" />
+      </div>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
