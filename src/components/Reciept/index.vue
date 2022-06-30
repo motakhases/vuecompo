780b37003f7b225b866baeed0bbc2af6f6c2c293
Shayeste <46010510+shayeste1996@users.noreply.github.com>
2022-06-30 23:46:21 +0430

    feat: create reciept component

---
+++ src/components/Reciept/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="zpl-reciept">
+    <div class="zpl-reciept-title">text</div>
+    <span class="zpl-reciept-divider" />
+    <div class="zpl-reciept-text">0 ریال</div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
---
+++ src/components/Reciept/logic.ts
@@ -0,0 +1,11 @@
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Icon from '@/components/Icon/index.vue';
+
+@Component({
+  components: { Icon },
+})
+export default class Reciept extends Vue {
+  @Prop({ type: String, required: true }) readonly label!: string
+
+  @Prop({ type: Boolean, default: false }) readonly btn!: boolean
+}
