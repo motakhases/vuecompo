8f1e3b422e2ae7f0d3397842b17d5a8dae772a0f
Ari Shojaei <amnshojaei@gmail.com>
2022-08-21 15:04:59 +0430

    feat: zp badge

---
+++ src/components/Zp/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <span :class="['zpl-zp', level]">
+    <!-- Level -->
+    <span class="zpl-zp-level">
+      <Icon name="ProfileFill" class="icon" />
+      {{ levelText }}
+    </span>
+
+    <!-- Id -->
+    <span class="zpl-zp-id">
+      ZP.{{ id }}
+    </span>
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
+
+export default Vue.extend({ mixins: [logic] });
+</script>
