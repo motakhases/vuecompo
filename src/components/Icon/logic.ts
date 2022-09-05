b9bd3be780a49552a621070b538016054c13afb8
Shayeste <46010510+shayeste1996@users.noreply.github.com>
2022-09-05 07:23:42 +0430

    feat: add excel icon

--- src/components/Icon/logic.ts
+++ src/components/Icon/logic.ts
@@ -92,6 +92,7 @@ import Refresh from './icons/Refresh.vue';
 import Telegram2 from './icons/Telegram2.vue';
 import AtSign from './icons/AtSign.vue';
 import CircleDelete from './icons/CircleDelete.vue';
+import ExcelColor from './icons/ExcelColor.vue';
 
 @Component({
   components: {
@@ -186,6 +187,7 @@ import CircleDelete from './icons/CircleDelete.vue';
     Telegram2,
     AtSign,
     CircleDelete,
+    ExcelColor,
   },
 })
 export default class Icon extends Vue {
