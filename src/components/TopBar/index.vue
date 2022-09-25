618b87ae5cc88337401e4b46ab5ac669e4ac6cac
Shayeste <46010510+shayeste1996@users.noreply.github.com>
2022-09-25 09:15:38 +0330

    fix: topbar

--- src/components/TopBar/index.vue
+++ src/components/TopBar/index.vue
@@ -54,9 +54,9 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import logic from "./logic";
-import "./style.scss";
+import Vue from 'vue';
+import logic from './logic';
+import './style.scss';
 
 export default Vue.extend({ mixins: [logic] });
 </script>
