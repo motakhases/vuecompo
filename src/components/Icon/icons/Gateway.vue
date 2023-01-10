d6428d39eaae4334debd07c5bb0abb4b32edd17d
premier213 <premierp1213@gmail.com>
2023-01-10 12:10:37 +0330

    feat: add icons

---
+++ src/components/Icon/icons/Gateway.vue
@@ -0,0 +1,68 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M19.5 21H4.5C3.395 21 2.5 20.105 2.5 19V5C2.5 3.895 3.395 3 4.5 3H19.5C20.605 3 21.5 3.895 21.5 5V19C21.5 20.105 20.605 21 19.5 21Z"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M2.5 8H21.5"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M10 14H14"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12 16V12"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M5.50488 5.49C5.50188 5.49 5.49988 5.492 5.49988 5.495C5.49988 5.498 5.50188 5.5 5.50488 5.5C5.50788 5.5 5.50988 5.498 5.50988 5.495C5.50988 5.492 5.50788 5.49 5.50488 5.49"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.04785 5.49C8.04485 5.49 8.04285 5.492 8.04285 5.495C8.04285 5.498 8.04485 5.5 8.04785 5.5C8.05085 5.5 8.05285 5.498 8.05285 5.495C8.05285 5.492 8.05085 5.49 8.04785 5.49"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M10.5879 5.49C10.5849 5.49 10.5829 5.492 10.5829 5.495C10.5829 5.498 10.5849 5.5 10.5879 5.5C10.5909 5.5 10.5929 5.498 10.5929 5.495C10.5929 5.492 10.5899 5.49 10.5879 5.49"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Gateway",
+});
+</script>
---
+++ src/components/Icon/icons/WebsiteBuild.vue
@@ -0,0 +1,104 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M19.2009 20.1734H4.80086C3.74006 20.1734 2.88086 19.3142 2.88086 18.2534V4.81343C2.88086 3.75263 3.74006 2.89343 4.80086 2.89343H19.2009C20.2617 2.89343 21.1209 3.75263 21.1209 4.81343V18.2534C21.1209 19.3142 20.2617 20.1734 19.2009 20.1734Z"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M2.88086 7.69338H21.1209"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M5.76613 5.28379C5.76325 5.28379 5.76133 5.28571 5.76133 5.28859C5.76133 5.29147 5.76325 5.29339 5.76613 5.29339C5.76901 5.29339 5.77093 5.29147 5.77093 5.28859C5.77093 5.28571 5.76901 5.28379 5.76613 5.28379"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.20656 5.28379C8.20368 5.28379 8.20176 5.28571 8.20176 5.28859C8.20176 5.29147 8.20368 5.29339 8.20656 5.29339C8.20944 5.29339 8.21136 5.29147 8.21136 5.28859C8.21136 5.28571 8.20944 5.28379 8.20656 5.28379"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M10.645 5.28379C10.6422 5.28379 10.6402 5.28571 10.6402 5.28859C10.6402 5.29147 10.6422 5.29339 10.645 5.29339C10.6479 5.29339 10.6498 5.29147 10.6498 5.28859C10.6498 5.28571 10.647 5.28379 10.645 5.28379"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12.0005 11.0535V12.0135"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12.0005 15.8534V16.8134"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9.50472 12.4934L10.3322 12.9734"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M13.6698 14.8934L14.4973 15.3734"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9.50472 15.3734L10.3322 14.8934"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M13.6698 12.9734L14.4973 12.4934"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M13.3583 12.576C14.108 13.3258 14.108 14.5411 13.3583 15.2909C12.6085 16.0406 11.3931 16.0406 10.6434 15.2909C9.89361 14.5411 9.89361 13.3258 10.6434 12.576C11.3931 11.8262 12.6085 11.8262 13.3583 12.576"
+      stroke="#393946"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "WebsiteBuild",
+});
+</script>
