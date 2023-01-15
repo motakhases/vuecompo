3f837f0b20f26b25929d6eb36318afac40a406ae
smhmp <gonbadkd@gmail.com>
2023-01-15 12:25:42 +0100

    ZDV-2753 Fix Overview page ZDV-2768 Update Share Modal component
    some bugs Fixed

--- src/components/Card/logic.ts
+++ src/components/Card/logic.ts
@@ -11,6 +11,8 @@ import Loading from './Loading/index.vue';
 export default class Card extends Vue {
   @Prop({ type: String }) readonly title!: string;
 
+  @Prop({ type: Boolean, default: false }) pinned!: boolean
+
   @Prop({ type: Boolean, default: false }) readonly loading?: boolean;
 
   hasContent = false
