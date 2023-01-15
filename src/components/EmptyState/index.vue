99d8dda4a177118c805bb199b171f27c06e1f43b
smhmp <gonbadkd@gmail.com>
2023-01-15 06:21:35 +0100

    ZDV-??? EmptyState Fixed on needs

--- src/components/EmptyState/index.vue
+++ src/components/EmptyState/index.vue
@@ -12,7 +12,7 @@
       <div class="zpl-emptystate-buttons" v-if="buttons.length">
         <Button
           v-for="(button, index) in buttons"
-          class="mx-2xs py-xs px-md"
+          class="mx-2xs py-xs px-md font-sans text-b2 text-right font-medium py-xs px-md"
           :key="index"
           :text="button.text"
           @click.native="button.action"
