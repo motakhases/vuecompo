5ba1affcbfb68b477f7e51ce7e6756989f5745f1
smhmp <gonbadkd@gmail.com>
2023-01-20 08:00:28 +0100

    my npm

--- dist/utils/filters/jalaali.js
+++
@@ -1,8 +0,0 @@
-import moment from 'moment-jalaali';
-
-const jalaali = (value) => {
-  const formatted = moment(value, 'YYYY-MM-DD HH:mm:ss').format(' jDD jMMMM jYYYY، HH:mm:ss ');
-  return value ? formatted : '---';
-};
-
-export default jalaali;
--- dist/utils/filters/numberFormat.js
+++
@@ -1,9 +0,0 @@
-const numberFormat = (value) => {
-  if (typeof value === 'undefined') {
-    return value;
-  }
-  // eslint-disable-next-line radix
-  return parseInt(value).toLocaleString('en');
-};
-
-export default numberFormat;
